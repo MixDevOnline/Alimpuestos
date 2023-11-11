2db0d940b19fe893c5a6e6464ec134a8a2a6f099
MixDevCode <66272629+MixDevCode@users.noreply.github.com>
2023-11-11 03:48:58 -0300

    prueba del nuevo sistema

--- assets/js/dolar.js
+++ assets/js/dolar.js
@@ -1,1 +1,53 @@
-function fetchDolarValue(){fetch("https://mercados.ambito.com/dolar/oficial/variacion").then(e=>e.json()).then(e=>{let t=e.venta,a=e.fecha.split(" ")[0];localStorage.setItem("valorDolar",t),localStorage.setItem("ultFecha",a),document.getElementById("dolar").textContent=`${t}`,document.getElementById("fecha").textContent=`(${a})`}).catch(e=>console.error("Error al obtener el valor del d\xf3lar: ",e))}function getStoredDolarValue(){let e=localStorage.getItem("valorDolar"),t=localStorage.getItem("ultFecha");e&&t?(document.getElementById("dolar").textContent=`${e}`,document.getElementById("fecha").textContent=`(${t})`):fetchDolarValue()}function shouldUpdateDolarValue(){let e=new Date,t=new Date(e);return t.setHours(15,0,0,0),e>=t}function updateDolarValueIfNeeded(){shouldUpdateDolarValue()?fetchDolarValue():getStoredDolarValue()}
+// Función para realizar la solicitud GET a la API
+function fetchDolarValues() {
+    fetch('https://horizontalinsidiousrepo.mixdevcode.repl.co/valores')
+        .then(response => response.json())
+        .then(data => {
+            // Almacenar los valores en el Local Storage
+            localStorage.setItem('dolarValues', JSON.stringify(data));
+
+            // Llamar a la función para construir el select
+            buildDolarSelect();
+        })
+        .catch(error => console.error('Error al obtener los valores del dólar: ', error));
+}
+
+// Función para obtener y llenar el select desde el Local Storage
+function buildDolarSelect() {
+    // Obtener los valores del dólar del Local Storage
+    const dolarValues = JSON.parse(localStorage.getItem('dolarValues'));
+    const bancoSelect = document.getElementById('bancoSelect');
+
+    // Verificar si hay datos disponibles y el elemento select existe
+    if (dolarValues && Array.isArray(dolarValues) && bancoSelect) {
+        // Limpiar el contenido actual del select
+        bancoSelect.innerHTML = '';
+
+        // Iterar sobre los datos y agregar opciones al select
+        dolarValues.forEach(value => {
+            const option = document.createElement('option');
+            option.value = value.value;
+            option.text = value.name;
+            bancoSelect.appendChild(option);
+        });
+    }
+}
+
+// Verificar si ya pasó la hora para actualizar el valor del dólar
+function shouldUpdateDolarValue() {
+    const horaActual = new Date();
+    const horaLimite = new Date(horaActual);
+    horaLimite.setHours(15, 0, 0, 0); // Establecer la hora límite a las 15:00
+
+    return true;
+}
+
+// Función principal para actualizar el valor del dólar si es necesario
+function updateDolarValueIfNeeded() {
+    if (shouldUpdateDolarValue()) {
+        fetchDolarValues();
+    } else {
+        // Llamar a la función para construir el select directamente
+        buildDolarSelect();
+    }
+}
